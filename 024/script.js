2ac7f0bd0b66a154f7001c11bba3b201104a0e56
BakufuXD <146445200+BakufuXD@users.noreply.github.com>
2026-06-29 16:19:08 +0700

    Add files via upload

---
+++ 024/script.js
@@ -0,0 +1,10 @@
+for(i=1;i<5;i++){
+    document.write("<h",i,">Computor Science @ NPRU <br></h",i,">");
+}
+for(i=1;i<=10;i++){
+    for(j=1;j<=i;j++){
+        document.write("<img src='head.jpg'width=60>");
+       
+    }
+    document.write("<br>");
+}
